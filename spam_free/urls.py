63f6ff9757493af60273b49ef4ed2c2e4ff606bd
anoopbk123 <anoopkavalakkal@gmail.com>
2023-04-18 06:03:49 +0530

    initial design of UI and database

--- spam_free/urls.py
+++ spam_free/urls.py
@@ -1,7 +1,7 @@
-"""spam_free URL Configuration
+"""spam_message URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,8 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from django.urls import  re_path, include
+from login import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path('chat/', include('chat.urls')),
+     re_path('comment/', include('comment.urls')),
+     re_path('friends/', include('friends.urls')),
+     re_path('like_photo/', include('like_photo.urls')),
+     re_path('login/', include('login.urls')),
+     re_path('reported_comments/', include('reported_comments.urls')),
+     re_path('share/', include('share.urls')),
+     re_path('user_reg/', include('user_reg.urls')),
+     re_path('temp/', include('temp.urls')),
+     re_path('$', views.login),
 ]
